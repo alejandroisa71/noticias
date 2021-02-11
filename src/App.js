c2047ac374845fcc305549854d5ea43330462cf8
Alejandro Isa <alejandroisa@gmail.com>
2021-02-11 15:36:53 -0300

    primer commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React,{Fragment} from 'react';
+import Formulario from './components/Formulario';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <Fragment>
+        <Header
+          titulo='Buscador de Noticias'
+        />
+
+        <div className="container white">
+            <Formulario/>
+        </div>
+    </Fragment>
+  );
+}
+
+export default App;
