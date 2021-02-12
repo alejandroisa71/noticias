f6c715181148433c6238598be52871595ff6e021
Alejandro Isa <alejandroisa@gmail.com>
2021-02-11 22:34:50 -0300

    se agregan componentes ListadoNoticias y Noticias y se consume la API newsapi.org

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,16 +1,36 @@
 import React from 'react';
 import styles from './Formulario.module.css';
 import useSelect from '../hooks/useSelect';
+import PropTypes from 'prop-types';
 
-const Formulario = () => {
+
+const Formulario = ({guardarCategoria}) => {
+
+    const OPCIONES = [
+        {value: 'general', label: 'General'},
+        {value: 'business', label: 'Negocio'},
+        {value: 'entertainment', label: 'Entretenimiento'},
+        {value: 'health', label: 'Salud'},
+        {value: 'science', label: 'Ciencia'},
+        {value: 'sports', label: 'Deportes'},
+        {value: 'technology', label: 'Teconología'}
+    ];
 
     //utilizar custom hook
-    const [categoria , SelectNoticias] = useSelect();
+    const [categoria , SelectNoticias] = useSelect('general', OPCIONES);
+
+    //submit al form, pasar categoria a app.js
+    const buscarNoticias = e =>{
+        e.preventDefault();
+        guardarCategoria(categoria);
+    }
 
     return ( 
         <div className={`${styles.buscador} row`}>
             <div className=" col s12 m8 offset-m2">
-                <form>
+                <form
+                    onSubmit={buscarNoticias}
+                >
                     <h2 className={styles.heading}>Encuentra Noticias por Categoria</h2>
 
                     <SelectNoticias/>
@@ -26,6 +46,10 @@ const Formulario = () => {
             </div>
         </div>
      );
+}
+
+Formulario.propTypes ={
+    guardarCategoria: PropTypes.func.isRequired
 }
  
 export default Formulario;
